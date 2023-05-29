0dc0f983921672c025a552f63e5201449693cb7b
Apoorv Pandey <apoorvpandey111@gmail.com>
2023-05-29 21:40:26 +0530

    Updated gitignore

--- lib/business_logic/use_case/auth_use_case/auth_use_case.dart
+++ lib/business_logic/use_case/auth_use_case/auth_use_case.dart
@@ -3,19 +3,19 @@ import 'package:task/data/remote_service/models/send_otp_response.dart';
 import 'package:task/data/remote_service/models/verify_otp_response.dart';
 
 class AuthUseCase {
-  final AuthRepo authRepoImpl;
+  final AuthRepo _authRepoImpl;
 
-  AuthUseCase({required this.authRepoImpl});
+  AuthUseCase({required AuthRepo authRepoImpl}) : _authRepoImpl = authRepoImpl;
 
   Future<SendOtpResponse> sendOTP(Map<String, String> body) {
-    return authRepoImpl.sendOTP(body);
+    return _authRepoImpl.sendOTP(body);
   }
 
   Future<VerifyOtpResponse> verifyOTP(Map<String, String> body) {
-    return authRepoImpl.verifyOTP(body);
+    return _authRepoImpl.verifyOTP(body);
   }
 
   Future<VerifyOtpResponse> submitProfile(Map<String, String> body) {
-    return authRepoImpl.submitProfile(body);
+    return _authRepoImpl.submitProfile(body);
   }
 }
